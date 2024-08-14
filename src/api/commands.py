f2c17108176cf6883b6e690062f7a4db5278af5e
José Hernández <josemiguelhernandezsousa@gmail.com>
2024-08-14 03:07:54 +0000

    Add /singup and /login and created some endpoints

--- src/api/commands.py
+++ src/api/commands.py
@@ -4,12 +4,12 @@ from api.models import db, User
 
 """
 In this file, you can add as many commands as you want using the @app.cli.command decorator
-Flask commands are usefull to run cronjobs or tasks outside of the API but sill in integration 
+Flask commands are usefull to run cronjobs or tasks outside of the API but sill in integration
 with youy database, for example: Import the price of bitcoin every night as 12am
 """
 def setup_commands(app):
-    
-    """ 
+
+    """
     This is an example command "insert-test-users" that you can run from the command line
     by typing: $ flask insert-test-users 5
     Note: 5 is the number of users to add
@@ -22,7 +22,6 @@ def setup_commands(app):
             user = User()
             user.email = "test_user" + str(x) + "@test.com"
             user.password = "123456"
-            user.is_active = True
             db.session.add(user)
             db.session.commit()
             print("User: ", user.email, " created.")
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,30 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route('/users', methods=['GET'])
+def getUsers():
+    users = User.query.all()
+    users = list(map(lambda item: item.serialize(), users))
+    return jsonify({'users': users})
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+@api.route('/users', methods=['POST'])
+def createUser():
+    data = request.json
+    user = User()
+    try:
+        user.email = data.get('email')
+        user.password = data.get('password')
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message":"user creado"})
+    except Exception as e:
+        print(e.args)
+        return jsonify({'message': 'Error'}),409
